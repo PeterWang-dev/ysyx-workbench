cb4658922a769bc1eda9c873250371a9d86a7da8
23060000-王嘉宸 <3121005231@mail2.gdut.edu.cn>
2023-11-21 20:38:25 +0800

    feat(tools/gen-expr): implement unit test

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -13,12 +13,35 @@
  * See the Mulan PSL v2 for more details.
  ******************************************************************************/
 
+#include "monitor/sdb/sdb.h"
 #include <common.h>
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+
+void test_expr(int argc, char *argv[]) {
+  if (argc < 2) {
+    panic("missing input file path");
+  }
+
+  FILE *fp = fopen(argv[1], "r");
+  uint32_t answer;
+  char e[65535];
+  while (fscanf(fp, "%u %s", &answer, e) != EOF) {
+    bool status = false;
+    word_t result = expr(e, &status);
+
+    if (answer != result) {
+      panic(
+          "test failed: answer %u is not equal to result %u when evaluating %s",
+          answer, result, e);
+    }
+  }
+
+  printf("success!\n");
+}
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -28,8 +51,14 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
+#ifdef CONFIG_TEST_EXPR
+  /* Test expr() */
+  test_expr(argc, argv);
+#else
+
   /* Start engine. */
   engine_start();
 
   return is_exit_status_bad();
+#endif
 }
