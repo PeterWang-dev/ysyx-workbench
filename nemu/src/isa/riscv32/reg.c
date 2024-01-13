be44a777389498a96cab03af615cd10dc63f49cf
tracer-ysyx <tracer@ysyx.org>
2024-01-13 20:56:31 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.6.10-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Fri, 05 Jan 2024 16:20:18 +0000 x86_64 GNU/Linux  20:56:31 up 1 day, 23:05,  1 user,  load average: 1.61, 1.26, 1.28

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -23,7 +23,7 @@ const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
 
 void isa_reg_display() {
   for (int i = 0; i < 32; i++) {
-    if (i > 0 && i < 5) {
+    if (i > 0 && i < 5) { // pointer registers using hex to display
       printf("%s\t0x%x\t0x%x\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
     } else {
       printf("%s\t0x%x\t%u\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
