834f4407dbee1a3e0cde959e7e3c039beeb24319
tracer-ysyx <tracer@ysyx.org>
2024-01-20 00:45:26 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.7.0-zen3-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 13 Jan 2024 14:36:54 +0000 x86_64 GNU/Linux  00:45:26 up  1:06,  1 user,  load average: 1.11, 1.06, 1.14

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -50,13 +50,14 @@ enum {
 
 #define immS()                                                                 \
   do {                                                                         \
-    *imm = (SEXT(BITS(i, 31, 25), 12) << 5) | BITS(i, 11, 7);                  \
+    *imm = SEXT((BITS(i, 31, 25) << 5) | BITS(i, 11, 7), 12);                  \
   } while (0)
 
 #define immB()                                                                 \
   do {                                                                         \
-    *imm = (SEXT(BITS(i, 31, 31), 12) << 12) | (BITS(i, 7, 7) << 11) |         \
-           (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1);                     \
+    *imm = SEXT((BITS(i, 31, 31) << 12) | (BITS(i, 7, 7) << 11) |              \
+                    (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1),            \
+                12);                                                           \
   } while (0)
 
 #define immU()                                                                 \
@@ -66,8 +67,9 @@ enum {
 
 #define immJ()                                                                 \
   do {                                                                         \
-    *imm = (SEXT(BITS(i, 31, 31), 20) << 20) | (BITS(i, 19, 12) << 12) |       \
-           (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1);                   \
+    *imm = SEXT(BITS(i, 31, 31) << 20 | (BITS(i, 19, 12) << 12) |              \
+                    (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1),          \
+                20);                                                           \
   } while (0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
