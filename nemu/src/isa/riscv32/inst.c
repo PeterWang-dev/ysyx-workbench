3a61e4dd04f3800557b7b4b1ccdb406ddb15b6ac
23060000-王嘉宸 <3121005231@mail2.gdut.edu.cn>
2024-01-21 16:03:59 +0800

    fix(decode_exec): fix potential bugs of sign-extending
    As previous commits, para of `SEXT` should be the exact shortest bits of the immediate.

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -57,19 +57,19 @@ enum {
   do {                                                                         \
     *imm = SEXT((BITS(i, 31, 31) << 12) | (BITS(i, 7, 7) << 11) |              \
                     (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1),            \
-                12);                                                           \
+                13);                                                           \
   } while (0)
 
 #define immU()                                                                 \
   do {                                                                         \
-    *imm = BITS(i, 31, 12) << 12;                                  \
+    *imm = SEXT(BITS(i, 31, 12) << 12, 32);                                    \
   } while (0)
 
 #define immJ()                                                                 \
   do {                                                                         \
     *imm = SEXT((BITS(i, 31, 31) << 20) | (BITS(i, 19, 12) << 12) |            \
                     (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1),          \
-                20);                                                           \
+                21);                                                           \
   } while (0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
