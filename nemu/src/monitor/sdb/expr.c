585f0f75d320f23f98a61cf200063914ef837c42
Peter Wang <59994316+PeterWang-dev@users.noreply.github.com>
2024-01-25 20:36:09 +0800

    Feature/pa2.1 (#5)
    * style: use clang-format to reformat the code
    
    * docs: add comment
    
    * build: add option to do test expr
    
    * chore: remove exhausive logs
    
    * feat(decode_exec): implement rv32i
    
    Some problems here:
    1. Need implement RV32M too;
    2. `string` related failed;
    3. `BAD TRAP` for `ebreak` in some tests;
    4. `BAD TRAP` should not `PASS` in test.
    
    * feat(decode_exec): implement rv32m
    
    * docs: add comments to split different isa ext
    
    * revert(decode_exec): some problems in rv32m, rollback via comment
    
    Test results:
    
    [  add-longlong] PASS!
    [           add] PASS!
    [           bit] PASS!
    [   bubble-sort] PASS!
    [         crc32] PASS!
    [           div] FAIL!
    [         dummy] PASS!
    [          fact] FAIL!
    [           fib] PASS!
    [      goldbach] FAIL!
    [     hello-str] FAIL!
    [       if-else] PASS!
    [     leap-year] FAIL!
    [    load-store] FAIL!
    [    matrix-mul] FAIL!
    [           max] PASS!
    [      mersenne] FAIL!
    [          min3] PASS!
    [         mov-c] PASS!
    [         movsx] PASS!
    [  mul-longlong] FAIL!
    [        pascal] PASS!
    [         prime] FAIL!
    [    quick-sort] FAIL!
    [     recursion] FAIL!
    [   select-sort] PASS!
    [         shift] PASS!
    [   shuixianhua] FAIL!
    [        string] FAIL!
    [  sub-longlong] PASS!
    [           sum] PASS!
    [        switch] PASS!
    [ to-lower-case] PASS!
    [       unalign] FAIL!
    [        wanshu] FAIL!
    
    * feat(sdb): add expr support for command x
    
    * fix(sdb/expr): fix find_op for new ops
    
    * fix(decode_exec): fix bugs
    
    1. fix rv32m:
       - should get upper `XLEN` bits instead of `XLEN` / 2 bits;
       - use `__(u)int128_t` for intermidiate result to prevent multiply overflow;
    2. fix wrong order of immediates sign-extending and shifting;
    3. fix wrong use of  `SEXT` as its second para should be the exact bits to sign-extend, as it is just a simple type conversion using exact highest bit as sign and converting back to unsigned to extend sign bit.
    
    Latest test still inferring bugs:
    [  add-longlong] PASS!
    [           add] PASS!
    [           bit] PASS!
    [   bubble-sort] PASS!
    [         crc32] FAIL!
    [           div] PASS!
    [         dummy] PASS!
    [          fact] PASS!
    [           fib] PASS!
    [      goldbach] PASS!
    [     hello-str] FAIL!
    [       if-else] PASS!
    [     leap-year] PASS!
    [    load-store] PASS!
    [    matrix-mul] PASS!
    [           max] PASS!
    [      mersenne] PASS!
    [          min3] PASS!
    [         mov-c] PASS!
    [         movsx] PASS!
    [  mul-longlong] PASS!
    [        pascal] PASS!
    [         prime] PASS!
    [    quick-sort] PASS!
    [     recursion] PASS!
    [   select-sort] PASS!
    [         shift] PASS!
    [   shuixianhua] PASS!
    [        string] FAIL!
    [  sub-longlong] PASS!
    [           sum] PASS!
    [        switch] PASS!
    [ to-lower-case] PASS!
    [       unalign] FAIL!
    [        wanshu] PASS!
    
    * ​fix(decode_exec): fix wrong immediate format of U-type
    ​
    U-immediate should not be sign-extended as shifting has move sign bit to appropriate position.
    
    ​Finish pa2.1. Resolve #4
    
    * fix(decode_exec): fix potential bugs of sign-extending
    
    As previous commits, para of `SEXT` should be the exact shortest bits of the immediate.

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -103,8 +103,8 @@ static bool basic_tokenize(char *expr) {
         char *substr_start = expr + pos;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i,
-            rules[i].regex, pos, substr_len, substr_len, substr_start);
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i,
+        //     rules[i].regex, pos, substr_len, substr_len, substr_start);
 
         pos += substr_len; // move
 
@@ -132,8 +132,8 @@ static bool basic_tokenize(char *expr) {
           tokens[cur_pos].type = rules[i].token_type;
           nr_token++;
 
-          Log("generated token %d: type: %d, contents: %s", nr_token,
-              tokens[cur_pos].type, tokens[cur_pos].str);
+          // Log("generated token %d: type: %d, contents: %s", nr_token,
+          //     tokens[cur_pos].type, tokens[cur_pos].str);
         }
 
         break;
@@ -194,7 +194,7 @@ static void post_tokenize() {
 }
 
 static bool make_token(char *e) {
-  int i;
+  // int i;
 
   if (!basic_tokenize(e)) {
     // error has been printed in trim_tokenize
@@ -203,11 +203,11 @@ static bool make_token(char *e) {
 
   post_tokenize();
 
-  Log("tokens are successfully generated");
-  Log("printing token info");
-  for (i = 0; i < nr_token; i++) {
-    Log("token[%d]: type: %d, content:%s", i, tokens[i].type, tokens[i].str);
-  }
+  // Log("tokens are successfully generated");
+  // Log("printing token info");
+  // for (i = 0; i < nr_token; i++) {
+  //   Log("token[%d]: type: %d, content:%s", i, tokens[i].type, tokens[i].str);
+  // }
 
   return true;
 }
@@ -247,7 +247,7 @@ static int find_op(int sp, int ep) {
            "spaces should not exists after making token");
 
     /* Rule 1: elems which is not an operator is definately not a main_op */
-    if (tokens[i].type == TK_NUM || tokens[i].type == TK_NEGTIVE) {
+    if (tokens[i].type == TK_REG || tokens[i].type == TK_NUM || tokens[i].type == TK_NEGTIVE) {
       // TK_NUM and TK_NEGTIVE are not operators
       continue;
     }
@@ -269,13 +269,37 @@ static int find_op(int sp, int ep) {
         main_op = tokens[i].type;
         op_index = i;
         break;
-      case TK_ADD:
-      case TK_SUB:
-        if (tokens[i].type == TK_ADD || tokens[i].type == TK_SUB) {
+      // Lowest level operator, only be updated when precedence is equal
+      // according to associativity
+      case TK_AND:
+        if (tokens[i].type == TK_AND) {
           main_op = tokens[i].type;
           op_index = i;
         }
         break;
+      // A bit lower than add and sub,
+      // only be updated when precedence is lower or equal
+      case TK_EQ:
+      case TK_NEQ:
+        if (tokens[i].type == TK_EQ || tokens[i].type == TK_NEQ ||
+            tokens[i].type == TK_AND) {
+          main_op = tokens[i].type;
+          op_index = i;
+        }
+        break;
+      // Lower than mul and div,
+      // only be updated when precedence is lower or equal
+      case TK_ADD:
+      case TK_SUB:
+        if (tokens[i].type == TK_ADD || tokens[i].type == TK_SUB ||
+            tokens[i].type == TK_EQ || tokens[i].type == TK_NEQ ||
+            tokens[i].type == TK_AND) {
+          main_op = tokens[i].type;
+          op_index = i;
+        }
+        break;
+      // Highest level operators, main_op always be updated
+      // referring to both precedence and associativity
       case TK_MUL:
       case TK_DIV:
         main_op = tokens[i].type;
