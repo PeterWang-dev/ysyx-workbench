c0ea8349410b7f06dfd2cdc0a2e40a73ee7ed310
tracer-ysyx <tracer@ysyx.org>
2024-02-06 23:40:22 +0800

    > generate verilog ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.7.3-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Fri, 02 Feb 2024 17:03:56 +0000 x86_64 GNU/Linux  23:40:22 up 23:11,  1 user,  load average: 2.74, 2.40, 2.43

--- npc/futurecore/sim/csrc/main.cpp
+++ npc/futurecore/sim/csrc/main.cpp
@@ -1,123 +1,54 @@
-// DESCRIPTION: Verilator: Verilog example module
-//
-// This file ONLY is placed under the Creative Commons Public Domain, for
-// any use, without warranty, 2017 by Wilson Snyder.
-// SPDX-License-Identifier: CC0-1.0
-//======================================================================
-
-// For std::unique_ptr
+#include "VFutureCore.h"
 #include <memory>
-
-// Include common routines
 #include <verilated.h>
 
-// Include model header, generated from Verilating "top.v"
-#include "Vtop.h"
-
-// Legacy function required only so linking works on Cygwin and MSVC++
 double sc_time_stamp() { return 0; }
 
-int main(int argc, char** argv) {
-    // This is a more complicated example, please also see the simpler examples/make_hello_c.
+int main(int argc, char **argv) {
+  Verilated::mkdir("logs");
 
-    // Create logs/ directory in case we have traces to put under it
-    Verilated::mkdir("logs");
+  // Create a simulation context for current model
+  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
 
-    // Construct a VerilatedContext to hold simulation time, etc.
-    // Multiple modules (made later below with Vtop) may share the same
-    // context to share time, or modules may have different contexts if
-    // they should be independent from each other.
+  // Set context options
+  contextp->debug(0);
+  contextp->randReset(2);
+  contextp->traceEverOn(true);
+  contextp->commandArgs(argc, argv);
 
-    // Using unique_ptr is similar to
-    // "VerilatedContext* contextp = new VerilatedContext" then deleting at end.
-    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-    // Do not instead make Vtop as a file-scope static variable, as the
-    // "C++ static initialization order fiasco" may cause a crash
+  // Construct the Verilated model
+  const std::unique_ptr<VFutureCore> top{
+      new VFutureCore{contextp.get(), "FutureCore"}};
 
-    // Set debug level, 0 is off, 9 is highest presently used
-    // May be overridden by commandArgs argument parsing
-    contextp->debug(0);
+  // Set initial signals
+  top->clk = 0;
 
-    // Randomization reset policy
-    // May be overridden by commandArgs argument parsing
-    contextp->randReset(2);
+  // Simulate until $finish
+  while (!contextp->gotFinish()) {
+    contextp->timeInc(1); // 1 timeprecision period passes...
 
-    // Verilator must compute traced signals
-    contextp->traceEverOn(true);
+    // Toggle a fast (time/2 period) clock
+    top->clk = !top->clk;
 
-    // Pass arguments so Verilated code can see them, e.g. $value$plusargs
-    // This needs to be called before you create any model
-    contextp->commandArgs(argc, argv);
+    // Evaluate model
+    top->eval();
 
-    // Construct the Verilated model, from Vtop.h generated from Verilating "top.v".
-    // Using unique_ptr is similar to "Vtop* top = new Vtop" then deleting at end.
-    // "TOP" will be the hierarchical name of the module.
-    const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
+    // Read outputs
+    VL_PRINTF("[%" PRId64 "] clk=%x rstl=%x iquad=%" PRIx64 " -> oquad=%" PRIx64
+              " owide=%x_%08x_%08x\n",
+              contextp->time(), top->clk, top->reset_l, top->in_quad,
+              top->out_quad, top->out_wide[2], top->out_wide[1],
+              top->out_wide[0]);
+  }
 
-    // Set Vtop's input signals
-    top->reset_l = !0;
-    top->clk = 0;
-    top->in_small = 1;
-    top->in_quad = 0x1234;
-    top->in_wide[0] = 0x11111111;
-    top->in_wide[1] = 0x22222222;
-    top->in_wide[2] = 0x3;
+  // Final model cleanup
+  top->final();
 
-    // Simulate until $finish
-    while (!contextp->gotFinish()) {
-        // Historical note, before Verilator 4.200 Verilated::gotFinish()
-        // was used above in place of contextp->gotFinish().
-        // Most of the contextp-> calls can use Verilated:: calls instead;
-        // the Verilated:: versions just assume there's a single context
-        // being used (per thread).  It's faster and clearer to use the
-        // newer contextp-> versions.
-
-        contextp->timeInc(1);  // 1 timeprecision period passes...
-        // Historical note, before Verilator 4.200 a sc_time_stamp()
-        // function was required instead of using timeInc.  Once timeInc()
-        // is called (with non-zero), the Verilated libraries assume the
-        // new API, and sc_time_stamp() will no longer work.
-
-        // Toggle a fast (time/2 period) clock
-        top->clk = !top->clk;
-
-        // Toggle control signals on an edge that doesn't correspond
-        // to where the controls are sampled; in this example we do
-        // this only on a negedge of clk, because we know
-        // reset is not sampled there.
-        if (!top->clk) {
-            if (contextp->time() > 1 && contextp->time() < 10) {
-                top->reset_l = !1;  // Assert reset
-            } else {
-                top->reset_l = !0;  // Deassert reset
-            }
-            // Assign some other inputs
-            top->in_quad += 0x12;
-        }
-
-        // Evaluate model
-        // (If you have multiple models being simulated in the same
-        // timestep then instead of eval(), call eval_step() on each, then
-        // eval_end_step() on each. See the manual.)
-        top->eval();
-
-        // Read outputs
-        VL_PRINTF("[%" PRId64 "] clk=%x rstl=%x iquad=%" PRIx64 " -> oquad=%" PRIx64
-                  " owide=%x_%08x_%08x\n",
-                  contextp->time(), top->clk, top->reset_l, top->in_quad, top->out_quad,
-                  top->out_wide[2], top->out_wide[1], top->out_wide[0]);
-    }
-
-    // Final model cleanup
-    top->final();
-
-    // Coverage analysis (calling write only after the test is known to pass)
+  // Coverage analysis (calling write only after the test is known to pass)
 #if VM_COVERAGE
-    Verilated::mkdir("logs");
-    contextp->coveragep()->write("logs/coverage.dat");
+  Verilated::mkdir("logs");
+  contextp->coveragep()->write("logs/coverage.dat");
 #endif
 
-    // Return good completion status
-    // Don't use exit() or destructor won't get called
-    return 0;
+  return 0;
 }
