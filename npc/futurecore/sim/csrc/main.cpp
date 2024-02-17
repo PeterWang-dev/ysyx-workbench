728cfbdfdbba88fc76e5105d0855cf2bdb2230da
tracer-ysyx <tracer@ysyx.org>
2024-02-17 21:25:31 +0800

    > sim RTL ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.7.4-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Mon, 05 Feb 2024 22:07:37 +0000 x86_64 GNU/Linux  21:25:31 up 20:18,  1 user,  load average: 2.11, 1.23, 0.77

--- npc/futurecore/sim/csrc/main.cpp
+++ npc/futurecore/sim/csrc/main.cpp
@@ -7,51 +7,51 @@ double sc_time_stamp() { return 0; }
 int main(int argc, char **argv) {
   Verilated::mkdir("logs");
 
-  // Create a simulation context for current model
-  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+//   // Create a simulation context for current model
+//   const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
 
-  // Set context options
-  contextp->debug(0);
-  contextp->randReset(2);
-  contextp->traceEverOn(true);
-  contextp->commandArgs(argc, argv);
+//   // Set context options
+//   contextp->debug(0);
+//   contextp->randReset(2);
+//   contextp->traceEverOn(true);
+//   contextp->commandArgs(argc, argv);
 
-  // Construct the Verilated model
-  const std::unique_ptr<VFutureCore> top{
-      new VFutureCore{contextp.get(), "FutureCore"}};
+//   // Construct the Verilated model
+//   const std::unique_ptr<VFutureCore> top{
+//       new VFutureCore{contextp.get(), "FutureCore"}};
 
-  // Set initial signals
-  top->clock = 0;
-  int runing_cycles = 10;
+//   // Set initial signals
+//   top->clock = 0;
+//   int runing_cycles = 10;
 
-  // Simulate until $finish
-  while (runing_cycles--) {
-    //   while (!contextp->gotFinish()) {
-    contextp->timeInc(1); // 1 timeprecision period passes...
+//   // Simulate until $finish
+//   while (runing_cycles--) {
+//     //   while (!contextp->gotFinish()) {
+//     contextp->timeInc(1); // 1 timeprecision period passes...
 
-    // Toggle a fast (time/2 period) clock
-    top->clock = !top->clock;
+//     // Toggle a fast (time/2 period) clock
+//     top->clock = !top->clock;
 
-    // Evaluate model
-    top->eval();
+//     // Evaluate model
+//     top->eval();
 
-    // Read outputs
-    // VL_PRINTF("[%" PRId64 "] clk=%x rstl=%x iquad=%" PRIx64 " -> oquad=%"
-    // PRIx64
-    //           " owide=%x_%08x_%08x\n",
-    //           contextp->time(), top->clk, top->reset_l, top->in_quad,
-    //           top->out_quad, top->out_wide[2], top->out_wide[1],
-    //           top->out_wide[0]);
-  }
+//     // Read outputs
+//     // VL_PRINTF("[%" PRId64 "] clk=%x rstl=%x iquad=%" PRIx64 " -> oquad=%"
+//     // PRIx64
+//     //           " owide=%x_%08x_%08x\n",
+//     //           contextp->time(), top->clk, top->reset_l, top->in_quad,
+//     //           top->out_quad, top->out_wide[2], top->out_wide[1],
+//     //           top->out_wide[0]);
+//   }
 
-  // Final model cleanup
-  top->final();
+//   // Final model cleanup
+//   top->final();
 
-// Coverage analysis (calling write only after the test is known to pass)
-#if VM_COVERAGE
-  Verilated::mkdir("logs");
-  contextp->coveragep()->write("logs/coverage.dat");
-#endif
+// // Coverage analysis (calling write only after the test is known to pass)
+// #if VM_COVERAGE
+//   Verilated::mkdir("logs");
+//   contextp->coveragep()->write("logs/coverage.dat");
+// #endif
 
   return 0;
 }
