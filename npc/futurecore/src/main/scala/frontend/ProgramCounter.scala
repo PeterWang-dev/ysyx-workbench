6ac96ed9599916406122f6e5b237df5a5f5c6a17
23060000-王嘉宸 <3121005231@mail2.gdut.edu.cn>
2024-02-20 17:48:26 +0800

    feat(frontend): implement program counter and test

--- npc/futurecore/src/main/scala/frontend/ProgramCounter.scala
+++ npc/futurecore/src/main/scala/frontend/ProgramCounter.scala
@@ -1,3 +1,22 @@
+/* Copyright PeterWang-dev (https://github.com/PeterWang-dev) 2024.
+
+  This source describes Open Hardware and is licensed under the CERN-OHL-W v2
+
+  You may redistribute and modify this documentation and make products
+  using it under the terms of the CERN-OHL-W v2 (https:/cern.ch/cern-ohl).
+
+  This documentation is distributed WITHOUT ANY EXPRESS OR IMPLIED
+  WARRANTY, INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY
+  AND FITNESS FOR A PARTICULAR PURPOSE. Please see the CERN-OHL-W v2
+  for applicable conditions.
+
+  Source location: https://www.ohwr.org/project/wr-switch-hw
+
+  As per CERN-OHL-W v2 section 4.1, should You produce hardware based on
+  these sources, You must maintain the Source Location visible on the
+  external case of the White Rabbit switch or other product you make using
+  this documentation.
+ */
 package futurecore.frontend
 
 import chisel3._
@@ -10,7 +29,7 @@ class ProgramCounter extends Module {
   val io        = IO(new ProgramCounterIO)
   val instWidth = 32.W
 
-  val pc        = RegInit(0.U(instWidth))
+  val pc = RegInit("h80000000".U(instWidth)) // PC reset to 0x80000000
   pc := pc + 4.U
 
   io.instAddr := pc
