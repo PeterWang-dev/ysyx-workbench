e4f849d2752b77342ca048e0ee940c9f8dc975bc
tracer-ysyx <tracer@ysyx.org>
2024-02-26 15:30:18 +0800

    > sim RTL ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.7.5-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 17 Feb 2024 14:02:21 +0000 x86_64 GNU/Linux  15:30:18 up 21:40,  5 users,  load average: 0.48, 0.41, 0.46

--- npc/FutureCore/src/main/resources/sim/vsrc/FutureCore.sv
+++ npc/FutureCore/src/main/resources/sim/vsrc/FutureCore.sv
@@ -292,9 +292,7 @@ module EbreakCall(
 
     //     end
     // end
-    $display("Before calling C Method");
     ebreak_call();
-    $display("After calling C Method");
 endmodule
 
 // ----- 8< ----- FILE "firrtl_black_box_resource_files.f" ----- 8< -----
