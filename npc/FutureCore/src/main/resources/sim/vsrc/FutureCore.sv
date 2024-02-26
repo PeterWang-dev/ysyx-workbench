cc1945bac003177459ba71b1684e8d0d3c9c9789
tracer-ysyx <tracer@ysyx.org>
2024-02-26 14:58:13 +0800

    > sim RTL ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.7.5-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 17 Feb 2024 14:02:21 +0000 x86_64 GNU/Linux  14:58:13 up 21:08,  5 users,  load average: 0.71, 0.71, 0.91

--- npc/FutureCore/src/main/resources/sim/vsrc/FutureCore.sv
+++ npc/FutureCore/src/main/resources/sim/vsrc/FutureCore.sv
@@ -285,7 +285,7 @@ import "DPI-C" function void ebreak_call();
 module EbreakCall(
     input clock,
     input reset,
-    input isEbreak,
+    input isEbreak
 );
     initial begin
         if (isEbreak) begin
