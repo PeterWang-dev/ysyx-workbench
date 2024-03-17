8f8bf2c20d2e7753f90ab51d305e992d02ff6d2a
23060000-王嘉宸 <3121005231@mail2.gdut.edu.cn>
2024-03-17 16:40:49 +0800

    feat(klib): implement simple sprintf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,27 +1,71 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
-#include <stdarg.h>
+#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+int printf(const char *format, ...) { panic("Not implemented"); }
+
+int sprintf(char *str, const char *format, ...) {
+  int cnt = 0;
+  va_list ap;
+
+  va_start(ap, format);
+
+  for (const char *ch = format; *ch != '\0'; ch++) {
+    switch (*ch) {
+    case '%': { // placeholder indicator
+      ch++;
+
+      switch (*ch) { // different format specifiers
+      case 'd': {
+        int num = va_arg(ap, int);
+        char *start = str, *end = itoa(num, str);
+        cnt += end - start;
+        str = end;
+        break;
+      }
+
+      case 's': {
+        const char *s = va_arg(ap, const char *);
+        while (*s != '\0') {
+          *str++ = *s++;
+          cnt++;
+        }
+
+        break;
+      }
+
+      default: {
+        panic("Not implemented");
+      }
+      }
+
+      break;
+    }
+
+    default: { // normal characters
+      *str++ = *ch;
+      cnt++;
+    }
+    }
+  }
+  *str = '\0'; // null-terminator on the end of output
+
+  va_end(ap);
+
+  return cnt;
+}
+
+int snprintf(char *str, size_t size, const char *format, ...) {
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int vsprintf(char *str, const char *format, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char *str, size_t size, const char *format, va_list ap) {
   panic("Not implemented");
 }
 
