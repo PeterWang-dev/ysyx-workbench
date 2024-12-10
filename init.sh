6e6fa90a47142912ba4fe077fd3e86113fa509ea
23060000-王嘉宸 <3121005231@mail2.gdut.edu.cn>
2024-12-10 12:58:01 +0800

    NJU-ProjectN/fceux-am ics2021 initialized using submodule
    NJU-ProjectN/fceux-am 9b167e7e47240cd8ef9a910242242c28affb713a fceu: remove unused function to fix linking error when SOUND_NONE

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 # usage: addenv env_name path
 function addenv() {
   sed -i -e "/^export $1=.*/d" ~/.bashrc
-  echo "export $1=`readlink -f $2`" >> ~/.bashrc
+  echo "export $1=$(readlink -f "$2")" >> ~/.bashrc
   echo "By default this script will add environment variables into ~/.bashrc."
   echo "After that, please run 'source ~/.bashrc' to let these variables take effect."
   echo "If you use shell other than bash, please add these environment variables manually."
@@ -12,29 +12,34 @@ function addenv() {
 # usage: init repo branch directory trace [env]
 # trace = true|false
 function init() {
-  if [ -d $3 ]; then
+  if [ -d "$3" ]; then
     echo "$3 is already initialized, skipping..."
     return
   fi
 
-  while [ ! -d $3 ]; do
-    git clone -b $2 git@github.com:$1.git $3
-  done
-  log="$1 `cd $3 && git log --oneline --no-abbrev-commit -n1`"$'\n'
+  while [ ! -d "$3" ]; do
+    if [ "$4" == "true" ]; then
+      git clone -b "$2" git@github.com:"$1".git "$3"
+    else
+      git submodule add -b "$2" git@github.com:"$1".git "$3"
+    fi
 
-  if [ $4 == "true" ] ; then
-    rm -rf $3/.git
-    git add -A $3
+  done
+  log="$1 $(cd "$3" && git log --oneline --no-abbrev-commit -n1)"$'\n'
+
+  if [ "$4" == "true" ] ; then
+    rm -rf "$3"/.git
+    git add -A "$3"
     git commit -am "$1 $2 initialized"$'\n\n'"$log"
   else
     sed -i -e "/^\/$3/d" .gitignore
     echo "/$3" >> .gitignore
     git add -A .gitignore
-    git commit --no-verify --allow-empty -am "$1 $2 initialized without tracing"$'\n\n'"$log"
+    git commit --no-verify --allow-empty -am "$1 $2 initialized using submodule"$'\n\n'"$log"
   fi
 
-  if [ $5 ] ; then
-    addenv $5 $3
+  if [ "$5" ] ; then
+    addenv "$5" "$3"
   fi
 }
 
