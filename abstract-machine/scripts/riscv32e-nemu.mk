bb0f93eb2ee96fb60cd364b1dff87e4b0d22a95e
tracer-ysyx <tracer@ysyx.org>
2025-04-10 12:43:11 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.13.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sun, 23 Mar 2025 17:17:17 +0000 x86_64 GNU/Linux  12:43:11  up   2:54,  5 users,  load average: 0.71, 0.85, 1.01

--- abstract-machine/scripts/riscv32e-nemu.mk
+++ abstract-machine/scripts/riscv32e-nemu.mk
@@ -1,7 +1,7 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
-override CROSS_COMPILE := riscv32-none-elf-# overwrite
+CROSS_COMPILE := riscv32-none-elf-                 # overwrite
 COMMON_CFLAGS += -march=rv32em_zicsr -mabi=ilp32e  # overwrite
 LDFLAGS       += -melf32lriscv                     # overwrite
 
