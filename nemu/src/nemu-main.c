70f57fb7ce684ee879028b4a51dd9f538d37addf
Peter Wang <59994316+PeterWang-dev@users.noreply.github.com>
2025-04-13 20:10:22 +0800

    fix(test_expr): improve success message format in test_expr function (#18)

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -15,6 +15,7 @@
  *******************************************************************************/
 
 #include "monitor/sdb/sdb.h"
+#include "utils.h"
 #include <common.h>
 
 void init_monitor(int, char *[]);
@@ -47,7 +48,7 @@ int test_expr(int argc, char *argv[]) {
     }
   }
 
-  printf("success!\n");
+  printf(ANSI_FMT("TEST EXPR SUCCESS\n", ANSI_FG_GREEN));
   return 0;
 }
 
