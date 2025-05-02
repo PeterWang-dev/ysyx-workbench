3709dc2a781a16fffcc0d7d8863be05fc92b3fb4
tracer-ysyx <tracer@ysyx.org>
2025-05-02 21:14:44 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.4-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 29 Apr 2025 09:23:22 +0000 x86_64 GNU/Linux  21:14:44  up 1 day  2:56,  4 users,  load average: 1.63, 1.27, 1.00

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,7 +4,7 @@
 void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint64_t ticks = (uint64_t)inl(RTC_ADDR + 4) << 32 | inl(RTC_ADDR);
+  uint64_t ticks = inl(RTC_ADDR) | (uint64_t)inl(RTC_ADDR + 4) << 32;
   uptime->us = ticks;
 }
 
