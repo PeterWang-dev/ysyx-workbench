e2fa96c216b5107b9bdd1e81c6839f0f2c81d34d
tracer-ysyx <tracer@ysyx.org>
2025-05-04 19:54:49 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.4-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 29 Apr 2025 09:23:22 +0000 x86_64 GNU/Linux  19:54:49  up   4:27,  5 users,  load average: 1.57, 1.59, 1.72

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,14 +4,23 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int screen_size = inl(VGACTL_ADDR);
+  int w = screen_size >> 16;
+  int h = screen_size & 0xffff;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++)
+    fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  uint32_t screen_size = inl(VGACTL_ADDR);
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = screen_size >> 16,
+                           .height = screen_size & 0xffff,
+                           .vmemsz = 0};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
@@ -20,6 +29,4 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
