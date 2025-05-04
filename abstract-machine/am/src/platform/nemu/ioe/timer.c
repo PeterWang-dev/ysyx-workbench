5f26659ca598b9ad372c41a18152c37661f964c0
tracer-ysyx <tracer@ysyx.org>
2025-05-04 09:53:01 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.4-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 29 Apr 2025 09:23:22 +0000 x86_64 GNU/Linux  09:53:01  up  21:02,  4 users,  load average: 0.75, 0.89, 0.89

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -7,10 +7,10 @@ void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   printf("Addr Ticks (High, Low): %d, %d\n", RTC_ADDR + 4, RTC_ADDR);
-  uint64_t lo = inl(RTC_ADDR);
-  printf("[AM]\tLow: %d\n", (uint32_t)lo);
   uint64_t hi = (uint64_t)inl(RTC_ADDR + 4) << 32;
   printf("[AM]\tTick High: %d\n", (uint32_t)(hi >> 32));
+  uint64_t lo = inl(RTC_ADDR);
+  printf("[AM]\tLow: %d\n", (uint32_t)lo);
   uptime->us = hi | lo;
   // uint64_t ticks = (uint64_t)inl(RTC_ADDR + 4) << 32 | inl(RTC_ADDR);
   // uptime->us = ticks;
