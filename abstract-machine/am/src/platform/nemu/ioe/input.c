76f5fbcc99c66ee202b21fc1d1aceff51076ac85
tracer-ysyx <tracer@ysyx.org>
2025-05-04 17:36:31 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.4-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 29 Apr 2025 09:23:22 +0000 x86_64 GNU/Linux  17:36:31  up   2:08,  5 users,  load average: 2.18, 1.94, 1.96

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,6 +7,9 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t am_scancode = inl(KBD_ADDR);
   bool is_keydown = (am_scancode & KEYDOWN_MASK);
+  if (!is_keydown) {
+    printf("UP!\n");
+  }
   kbd->keydown = is_keydown ? 1 : 0;
   kbd->keycode = is_keydown ? (am_scancode & ~KEYDOWN_MASK) : AM_KEY_NONE;
 }
