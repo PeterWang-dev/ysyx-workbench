e25601b1b875755b54be405d9cda70d993d44b7a
Peter Wang <59994316+PeterWang-dev@users.noreply.github.com>
2025-05-05 10:37:20 +0800

    Feature/pa2.3 (#20)
    * chore: use submodule for futurecore instead of integrating into workbench
    
    * fix(riscv.mk): change compiler to rv32-none-elf to use nix provided compiler
    
    * feat(npc.mk): add basic rules to sim npc with image
    
    * feat(npc/trm.c): add npc trap
    
    * chore: change npc to submodule
    
    * feat(klib/stdio.c): correct and implement printf and sprintf functions
    
    * refactor(reg.c): simplify isa_reg_display function to output hex only
    
    * style(device/io/map.c): format comments and improve code readability
    
    * fix(device/timer.c): tick should be invoked both for 0 and 4 offset
    
    * fix(klib/stdlib.c): correct itoa function to handle zero and negative values properly
    
    * feat(platform/nemu/timer.c): implement uptime timer
    
    * feat(stdlib.c): implement simple malloc
    
    * feat: add device tracing support and improve logging format
    
    * fix(platform/nemu/ioe/input.c): implement keyboard
    
    * fix(device/io/map.c): add missing space in dtrace for read operation
    
    * feat: implement gpu
    
    * fix(klib.h): use glibc on native

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -24,7 +24,7 @@
 extern "C" {
 #endif
 
-#define __NATIVE_USE_KLIB__
+// #define __NATIVE_USE_KLIB__ // native use glibc as we implemented malloc
 
 // string.h
 void *memcpy(void *dest, const void *src, size_t n);
