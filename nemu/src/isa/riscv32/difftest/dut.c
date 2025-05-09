6ecaade1ac9e7f7a7e3bae0b54e08c5d89ec9f4f
tracer-ysyx <tracer@ysyx.org>
2025-05-10 01:06:20 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.5-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 03 May 2025 13:33:50 +0000 x86_64 GNU/Linux  01:06:20  up 1 day 13:12,  5 users,  load average: 1.73, 1.72, 1.60

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,10 +20,35 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
     if (gpr(i) != ref_r->gpr[i]) {
-      // Log("Mismatch at gpr[%d]: DUT = " FMT_WORD ", REF = " FMT_WORD, i, gpr(i),
+      // Log("Mismatch at gpr[%d]: DUT = " FMT_WORD ", REF = " FMT_WORD, i,
+      // gpr(i),
       //     ref_r->gpr[i]);
       return false;
     }
+  }
+
+  if (cpu.csr[MTVEC] != ref_r->csr[MTVEC]) {
+    // Log("Mismatch at csr[MTVEC]: DUT = " FMT_WORD ", REF = " FMT_WORD,
+    //     cpu.csr[MTVEC], ref_r->csr[MTVEC]);
+    return false;
+  }
+
+  if (cpu.csr[MEPC] != ref_r->csr[MEPC]) {
+    // Log("Mismatch at csr[MEPC]: DUT = " FMT_WORD ", REF = " FMT_WORD,
+    //     cpu.csr[MEPC], ref_r->csr[MEPC]);
+    return false;
+  }
+
+  if (cpu.csr[MCAUSE] != ref_r->csr[MCAUSE]) {
+    // Log("Mismatch at csr[MCAUSE]: DUT = " FMT_WORD ", REF = " FMT_WORD,
+    //     cpu.csr[MCAUSE], ref_r->csr[MCAUSE]);
+    return false;
+  }
+
+  if (cpu.csr[MSTATUS] != ref_r->csr[MSTATUS]) {
+    // Log("Mismatch at csr[MSTATUS]: DUT = " FMT_WORD ", REF = " FMT_WORD,
+    //     cpu.csr[MSTATUS], ref_r->csr[MSTATUS]);
+    return false;
   }
 
   if (cpu.pc != ref_r->pc) {
