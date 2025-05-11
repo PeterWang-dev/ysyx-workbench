4827a1368b827c14a0993941c10865bd234f4874
tracer-ysyx <tracer@ysyx.org>
2025-05-11 19:13:18 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.5-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 03 May 2025 13:33:50 +0000 x86_64 GNU/Linux  19:13:18  up   7:48,  5 users,  load average: 1.55, 2.24, 1.72

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,7 +18,7 @@
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   //! Set MSTATUS to 0x1800 because we are not using it but need to pass
   //! difftest with Spike, so keep it consistent with Spike.
-  // cpu.csr[MSTATUS] = 0x1800;
+  cpu.csr[MSTATUS] = 0x1800;
   cpu.csr[MEPC] = epc;
   cpu.csr[MCAUSE] = NO;
   IFDEF(CONFIG_ETRACE, log_write("[etrace] exception triggerd at " FMT_PADDR
