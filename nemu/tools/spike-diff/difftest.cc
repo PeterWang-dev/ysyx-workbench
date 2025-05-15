7f344cf34e6337e350a6981028e072ba78497e80
tracer-ysyx <tracer@ysyx.org>
2025-05-15 16:27:26 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.6-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Fri, 09 May 2025 17:35:59 +0000 x86_64 GNU/Linux  16:27:26  up  16:57,  6 users,  load average: 2.05, 1.70, 1.66

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -70,10 +70,10 @@ void sim_t::diff_set_regs(void *diff_context) {
   for (int i = 0; i < NR_GPR; i++) {
     state->XPR.write(i, (sword_t)ctx->gpr[i]);
   }
-  state->mstatus->write(ctx->csr[0x300]); // mstatus
-  state->mtvec->write(ctx->csr[0x305]);   // mtvec
-  state->mepc->write(ctx->csr[0x341]);    // mepc
-  state->mcause->write(ctx->csr[0x342]);  // mcause
+  // state->mstatus->write(ctx->csr[0x300]); // mstatus
+  // state->mtvec->write(ctx->csr[0x305]);   // mtvec
+  // state->mepc->write(ctx->csr[0x341]);    // mepc
+  // state->mcause->write(ctx->csr[0x342]);  // mcause
   state->pc = ctx->pc;
 }
 
