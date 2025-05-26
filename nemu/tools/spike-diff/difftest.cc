3a5d1cf9c3ce7de120a1af628ed4fbf2df1beb09
tracer-ysyx <tracer@ysyx.org>
2025-05-26 17:51:33 +0800

    > generate verilog ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.7-zen2-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 22 May 2025 05:37:13 +0000 x86_64 GNU/Linux  17:51:33  up   6:44,  5 users,  load average: 0.92, 1.42, 1.36

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -38,7 +38,6 @@ static debug_module_config_t difftest_dm_config = {
 
 struct diff_context_t {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
-  word_t csr[4096];
   word_t pc;
 };
 
@@ -60,10 +59,6 @@ void sim_t::diff_get_regs(void* diff_context) {
   for (int i = 0; i < NR_GPR; i++) {
     ctx->gpr[i] = state->XPR[i];
   }
-  ctx->csr[0x300] = state->mstatus->read(); // mstatus
-  ctx->csr[0x305] = state->mtvec->read();   // mtvec
-  ctx->csr[0x341] = state->mepc->read();    // mepc
-  ctx->csr[0x342] = state->mcause->read();  // mcause
   ctx->pc = state->pc;
 }
 
@@ -72,10 +67,6 @@ void sim_t::diff_set_regs(void* diff_context) {
   for (int i = 0; i < NR_GPR; i++) {
     state->XPR.write(i, (sword_t)ctx->gpr[i]);
   }
-  state->mstatus->write(ctx->csr[0x300]); // mstatus
-  state->mtvec->write(ctx->csr[0x305]);   // mtvec
-  state->mepc->write(ctx->csr[0x341]);    // mepc
-  state->mcause->write(ctx->csr[0x342]);  // mcause
   state->pc = ctx->pc;
 }
 
@@ -136,4 +127,5 @@ __EXPORT void difftest_raise_intr(uint64_t NO) {
   trap_t t(NO);
   p->take_trap_public(t, state->pc);
 }
+
 }
