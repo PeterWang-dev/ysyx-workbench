c176412d27f9c47843780457d8c1e97f5897a1f3
tracer-ysyx <tracer@ysyx.org>
2025-06-11 01:59:17 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.14.10-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jun 2025 18:52:21 +0000 x86_64 GNU/Linux  01:59:17  up 1 day  8:55,  6 users,  load average: 1.49, 1.68, 1.79

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -47,7 +47,8 @@ void yield() {
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
 #else
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a7, -1; ecall"); //! BUG: DEAD HERE! As epc is set here after
+                                    //! first time two process iterating.
 #endif
 }
 
