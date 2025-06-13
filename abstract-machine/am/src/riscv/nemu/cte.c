d9aea062d701da5a5f0a2d9b8666ad32e1cbcbad
tracer-ysyx <tracer@ysyx.org>
2025-06-14 01:15:00 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.15.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 10 Jun 2025 21:32:14 +0000 x86_64 GNU/Linux  01:15:00  up  12:43,  5 users,  load average: 1.75, 1.83, 1.91

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -18,7 +18,6 @@ Context *__am_irq_handle(Context *c) {
     }
 
     c = user_handler(ev, c);
-    printf("Reached here!\n");
     assert(c != NULL);
   }
 
@@ -39,6 +38,7 @@ bool cte_init(Context *(*handler)(Event, Context *)) {
 
 Context *kcontext(Area kstack, void (*sentry)(void *), void *arg) {
   Context *c = (Context *)kstack.end - sizeof(Context);
+  printf("Reached here! c:%d\n", (uintptr_t)c);
   assert(c >= (Context *)kstack.start);
   c->mstatus = 0x1800;
   c->mepc = (uintptr_t)sentry - 4;
