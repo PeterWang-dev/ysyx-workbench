ed12d955359e4c36bebe4f2b41e8c63cd7f85478
tracer-ysyx <tracer@ysyx.org>
2026-01-04 00:59:46 +0800

    > compile NEMU ysyx_23060000 王嘉宸 Linux PeterWang-ArchLinux 6.18.2-zen2-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 18 Dec 2025 18:00:58 +0000 x86_64 GNU/Linux  00:59:46  up  10:14,  6 users,  load average: 4.84, 3.46, 2.79

--- nemu/default.nix
+++ nemu/default.nix
@@ -3,24 +3,28 @@
   llvmPackages,
   bison,
   flex,
+  dtc,
+  boost,
   readline,
   SDL2,
   SDL2_ttf,
   SDL2_image,
-  dtc,
 }:
 stdenv.mkDerivation {
   pname = "nemu";
   version = "0.1.0";
   src = ./.;
   nativeBuildInputs = [
+    # For menuconfig
     bison
     flex
+    # For difftest Spike compilation
+    dtc
+    boost
   ];
   buildInputs = [
     llvmPackages.llvm
     readline
-    dtc
     SDL2
     SDL2_ttf
     SDL2_image
