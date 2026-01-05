be0d670769bf6a684354cff99ae853cf38b05676
Peter Wang <59994316+PeterWang-dev@users.noreply.github.com>
2026-01-05 14:52:57 +0800

    Chore/simplify nix shell (#25)
    * chore: simplify the nix-shell
    
    * chore: keep in track of vscode settings
    
    * chore(am): add nix dev environment
    
    * chore: bump submodules

---
+++ nemu/default.nix
@@ -0,0 +1,34 @@
+{
+  stdenv,
+  llvmPackages,
+  bison,
+  flex,
+  dtc,
+  boost,
+  readline,
+  SDL2,
+  SDL2_ttf,
+  SDL2_image,
+}:
+stdenv.mkDerivation {
+  pname = "nemu";
+  version = "0.1.0";
+  src = ./.;
+  nativeBuildInputs = [
+    # For menuconfig
+    bison
+    flex
+    # For difftest Spike compilation
+    dtc
+    boost
+    # NEMU build dependency
+    llvmPackages.bintools # enable the ability to use correct rpath
+  ];
+  buildInputs = [
+    llvmPackages.llvm # for disassemble support
+    readline
+    SDL2
+    SDL2_ttf
+    SDL2_image
+  ];
+}
