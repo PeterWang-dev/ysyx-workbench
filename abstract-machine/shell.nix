a5297ab3778ce4b2703a5c758dc1f78e4137a794
Peter Wang <59994316+PeterWang-dev@users.noreply.github.com>
2026-03-06 00:54:13 +0800

    Revert "fix(nix-shell): correct argument passing and cross package set (#27)"
    This reverts commit eae8eddc0ff17d511dd2b3a92032131b028124ef.

--- abstract-machine/shell.nix
+++ abstract-machine/shell.nix
@@ -1,20 +1,15 @@
 {
   pkgs ? import (fetchTarball "https://nixos.org/channels/nixos-25.11/nixexprs.tar.xz") { },
-  nemu-env,
-  npc-env,
+  nemu-env ? null,
+  npc-env ? null,
 }:
 let
-  # ! Must use buildPackages otherwise LD_LIBRARY_PATH would be interfered
-  pkgsCross = pkgs.pkgsCross.riscv32.buildPackages;
+  pkgsCross = pkgs.pkgsCross.riscv32;
   am = pkgsCross.callPackage ./default.nix { };
 in
 pkgs.mkShell rec {
   name = "am-env";
-  inputsFrom = [
-    am
-    nemu-env
-    npc-env
-  ];
+  inputsFrom = [ am ];
   packages = [ ];
   shellHook = ''
     export AM_HOME=${AM_HOME}
